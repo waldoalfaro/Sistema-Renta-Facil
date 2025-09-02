20c3c642d5a31426ad38d1844a248d8aa2c6bb7a
venis12 <nvenismoran@gmail.com>
2025-09-01 20:39:04 -0600

    SR-8-terminado

---
+++ Usuarios/Usuarios.php
@@ -0,0 +1,115 @@
+<?php
+include "../menu.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="p-4 sm:ml-64">
+<div class="container mt-5">
+    <a href="#" class="btn btn-success mb-3" data-bs-toggle="modal" data-bs-target="#ModalRegUsuario">
+        <i class="fa-solid fa-user-plus"></i> Agregar Usuario
+    </a>
+
+</div>
+
+<table class="table table-hover table-bordered text-center" style="margin-top: 20px;">
+    <thead class="bg-info text-white">
+        <tr>
+            <th>N</th>
+            <th>Nombre</th>
+            <th>Usuario</th>
+            <th>Email</th>
+            <th colspan="2">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php  ?>
+            <tr>
+               
+            </tr>
+        <?php  ?>
+    </tbody>
+</table>
+
+
+<div class="modal fade" id="ModalRegUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Agregar Usuario</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="agregar.php" method="POST">
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Usuario</label>
+                        <input type="text" class="form-control" id="usuario" name="usuario" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo</label>
+                        <input type="email" class="form-control" id="email" name="correo" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Tipo Usuario</label>
+                        <select class="form-control" name="tipo_usuario" id="tipo_usuario" required>
+                            <option value="">Seleccione un tipo</option>
+                            
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    
+    var editModal = document.getElementById('ModalEditUsuario');
+    editModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget; 
+        var id = button.getAttribute('data-id');
+        var usuario = button.getAttribute('data-usuario');
+        var email = button.getAttribute('data-email');
+        var tipo = button.getAttribute('data-tipo');
+
+        
+
+        modalId.value = id;
+        modalUsuario.value = usuario;
+        modalEmail.value = email;
+        modalTipo.value = tipo;
+    });
+
+
+    
+
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+</div>
+    
+
+
+
